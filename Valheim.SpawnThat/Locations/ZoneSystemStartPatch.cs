4f96bf17bf97a17df23373f0e1a6e2708889e3d9
Asharppen <asharppen@gmail.com>
2021-04-20 20:17:32 +0200

    Rewired location retrieval to wait for peer request.

---
+++ Valheim.SpawnThat/Locations/ZoneSystemStartPatch.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Valheim.SpawnThat.Locations
+{
+    [HarmonyPatch(typeof(ZoneSystem))]
+    public static class ZoneSystemStartPatch
+    {
+        [HarmonyPatch("Start")]
+        [HarmonyPostfix]
+        private static void LoadLocations(Dictionary<Vector2i, ZoneSystem.LocationInstance> ___m_locationInstances)
+        {
+            if (___m_locationInstances is not null && ___m_locationInstances.Count > 0)
+            {
+                LocationHelper.SetLocationInstances(___m_locationInstances);
+            }
+        }
+    }
+}
