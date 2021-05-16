cf966cd9fbcc5f283edb84fc9271c0470d6c6423
Asharppen <asharppen@gmail.com>
2021-05-16 21:59:06 +0200

    Optimized and compressed locations and config sync

--- Valheim.SpawnThat/Locations/SimpleLocation.cs
+++ Valheim.SpawnThat/Locations/SimpleLocation.cs
@@ -11,39 +11,4 @@ namespace Valheim.SpawnThat.Locations
 
         public string LocationName;
     }
-
-    [Serializable]
-    public struct SimpleLocationDTO
-    {
-        public int ZonePositionX;
-        public int ZonePositionY;
-
-        public float PositionX;
-        public float PositionY;
-        public float PositionZ;
-
-        public string LocationName;
-
-        public SimpleLocationDTO(Vector2i zonePos, Vector3 pos, string location)
-        {
-            ZonePositionX = zonePos.x;
-            ZonePositionY = zonePos.y;
-
-            PositionX = pos.x;
-            PositionY = pos.y;
-            PositionZ = pos.z;
-
-            LocationName = location;
-        }
-
-        public SimpleLocation ToSimpleLocation()
-        {
-            return new SimpleLocation
-            {
-                Position = new Vector3(PositionX, PositionY, PositionZ),
-                ZonePosition = new Vector2i(ZonePositionX, ZonePositionY),
-                LocationName = LocationName
-            };
-        }
-    }
 }
