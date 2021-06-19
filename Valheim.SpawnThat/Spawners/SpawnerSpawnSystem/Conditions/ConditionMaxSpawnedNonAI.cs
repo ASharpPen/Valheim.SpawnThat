4895e1bb5a1f0dc9eabaded4b2010a71714e5719
Asharppen <asharppen@gmail.com>
2021-06-19 18:55:47 +0200

    Fixed MaxSpawned and SpawnDistance for non-ai prefabs

---
+++ Valheim.SpawnThat/Spawners/SpawnerSpawnSystem/Conditions/ConditionMaxSpawnedNonAI.cs
@@ -0,0 +1,76 @@
+﻿// <summary>
+// Unnecessary as long as the instance count itself is patched.
+// </summary>
+
+#if FALSE
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Valheim.SpawnThat.Configuration.ConfigTypes;
+using Valheim.SpawnThat.Core;
+using Valheim.SpawnThat.Spawners.Caches;
+using Valheim.SpawnThat.Spawners.SpawnerSpawnSystem.Managers;
+
+namespace Valheim.SpawnThat.Spawners.SpawnerSpawnSystem.Conditions
+{
+
+    public class ConditionMaxSpawnedNonAI : IConditionOnSpawn
+    {
+        private static ConditionMaxSpawnedNonAI _instance;
+
+        public static ConditionMaxSpawnedNonAI Instance => _instance ??= new();
+
+        public bool ShouldFilter(SpawnConditionContext context)
+        {
+            var counter = SpawnSessionManager.Instance.GetService<SpawnCounter>();
+
+            if (counter is null)
+            {
+                // Something is wrong. Skip this condition.
+                return false;
+            }
+
+            if (IsValid(context.SpawnData.m_prefab, context.Config, counter))
+            {
+                return false;
+            }
+
+            Log.LogTrace($"Ignoring world config {context.Config.Name} due to too many entities spawned in area.");
+            return true;
+        }
+
+        public bool IsValid(GameObject prefab, SpawnConfiguration config, SpawnCounter counter)
+        {
+            if(config is null || config.MaxSpawned.Value < 0)
+            {
+                return true;
+            }
+
+            if (config.MaxSpawned.Value == 0)
+            {
+                return false;
+            }
+
+            // Ignore if prefab is a normal creature / AI.
+            var baseAI = PrefabCache.GetBaseAI(prefab);
+            if (baseAI || baseAI is not null)
+            {
+                return true;
+            }
+
+            int instances = counter.CountInstancesInRange(prefab);
+
+            if (instances < config.MaxSpawned.Value)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
+#endif
---
+++ Valheim.SpawnThat/Spawners/SpawnerSpawnSystem/Patches/SpawnSystemSpawnSessionPatch.cs
@@ -0,0 +1,30 @@
+﻿using HarmonyLib;
+using Valheim.SpawnThat.Core;
+using Valheim.SpawnThat.Spawners.SpawnerSpawnSystem.Managers;
+
+namespace Valheim.SpawnThat.Spawners.SpawnerSpawnSystem.Patches
+{
+    [HarmonyPatch(typeof(SpawnSystem))]
+    internal static class SpawnSystemSpawnSessionPatch
+    {
+        [HarmonyPatch(nameof(SpawnSystem.UpdateSpawnList))]
+        [HarmonyPrefix]
+        private static void InitSpawnSession(SpawnSystem __instance)
+        {
+            SpawnSessionManager.ResetSession();
+
+#if DEBUG
+            Log.LogDebug($"Starting new spawn session at {__instance.transform.position}");
+#endif
+            // 250 is a rough guess, but it seems about right for the currently loaded area distance.
+            SpawnSessionManager.Instance.SetService(new SpawnCounter(__instance.transform.position, 250));
+        }
+
+        [HarmonyPatch(nameof(SpawnSystem.UpdateSpawnList))]
+        [HarmonyPostfix]
+        private static void EndSpawnSession()
+        {
+            SpawnSessionManager.ResetSession();
+        }
+    }
+}
