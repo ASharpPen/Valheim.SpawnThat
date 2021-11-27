02fefc72983ea1bea2879e445c22fb8b4c494d73
Asharppen <asharppen@gmail.com>
2021-11-27 22:23:10 +0100

    Version bump and documentation

--- Valheim.SpawnThat/SpawnThatPlugin.cs
+++ Valheim.SpawnThat/SpawnThatPlugin.cs
@@ -8,7 +8,7 @@ namespace Valheim.SpawnThat
     [BepInDependency("RagnarsRokare.MobAILib", BepInDependency.DependencyFlags.SoftDependency)]
     [BepInDependency("org.bepinex.plugins.creaturelevelcontrol", BepInDependency.DependencyFlags.SoftDependency)]
     [BepInDependency("randyknapp.mods.epicloot", BepInDependency.DependencyFlags.SoftDependency)]
-    [BepInPlugin("asharppen.valheim.spawn_that", "Spawn That!", "0.11.5")]
+    [BepInPlugin("asharppen.valheim.spawn_that", "Spawn That!", "0.11.6")]
     public class SpawnThatPlugin : BaseUnityPlugin
     {        
         // Awake is called once when both the game and the plug-in are loaded
