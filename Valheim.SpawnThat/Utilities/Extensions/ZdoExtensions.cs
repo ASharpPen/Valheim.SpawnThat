e4f82e22f5d78d67dafda6987390b6da1142a929
Asharppen <asharppen@gmail.com>
2022-01-04 18:36:44 +0100

    Added zdo extensions

---
+++ Valheim.SpawnThat/Utilities/Extensions/ZdoExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace Valheim.SpawnThat.Utilities.Extensions;
+
+public static class ZdoExtensions
+{
+    private static int NoiseHash = "noise".GetStableHashCode();
+    private static int TamedHash = "tamed".GetStableHashCode();
+    private static int EventCreatureHash = "EventCreature".GetStableHashCode();
+
+    public static float GetNoise(this ZDO zdo)
+    {
+        return zdo.GetFloat(NoiseHash);
+    }
+
+    public static bool GetTamed(this ZDO zdo)
+    {
+        return zdo.GetBool(TamedHash);
+    }
+
+    public static bool GetEventCreature(this ZDO zdo)
+    {
+        return zdo.GetBool(EventCreatureHash);
+    }
+}
