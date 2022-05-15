a322fa300e424c4b9b996fb81f5488afc0980cbf
Asharppen <asharppen@gmail.com>
2022-05-16 00:01:47 +0200

    Initial merging of matching destructible spawners on build

---
+++ src/SpawnThat/Spawners/DestructibleSpawner/Configuration/BuilderOption.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace SpawnThat.Spawners.DestructibleSpawner.Configuration;
+
+/// <summary>
+/// Simple wrapper to detect if an option has been assigned or not.
+/// 
+/// This is intended to  help identify intentional null values, from simply never assigned ones.
+/// </summary>
+internal class BuilderOption<T>
+{
+    public T Value { get; set; }
+
+    public BuilderOption(T value)
+    {
+        Value = value;
+    }
+}
+
+/*
+internal class BuilderOption<T>
+{
+    public bool IsSet { get; private set; }
+
+    public T Value { get; private set; }
+
+    public void Set(T value)
+    {
+        IsSet = true;
+        Value = value;
+    }
+}
+*/
+
+internal static class BuilderOptionExtensions
+{
+    public static void DoIfSet<T>(this BuilderOption<T> option, Action<T> action)
+    {
+        if (option is not null)
+        {
+            action(option.Value);
+        }
+    }
+
+    public static void DoIfSet<T>(this BuilderOption<T> option, Action<BuilderOption<T>> action)
+    {
+        if (option is not null)
+        {
+            action(option);
+        }
+    }
+
+    public static void AssignIfSet<T>(this BuilderOption<T> source, ref BuilderOption<T> target)
+    {
+        if (source is not null)
+        {
+            target = source;
+        }
+    }
+}
--- src/SpawnThat/Spawners/DestructibleSpawner/Configuration/DestructibleSpawnerBuilder.cs
+++ src/SpawnThat/Spawners/DestructibleSpawner/Configuration/DestructibleSpawnerBuilder.cs
@@ -9,21 +9,21 @@ internal class DestructibleSpawnerBuilder : IDestructibleSpawnerBuilder
 {
     private DestructibleSpawnerTemplate Template { get; } = new();
 
-    private Dictionary<uint, DestructibleSpawnBuilder> Spawns { get; } = new();
+    internal Dictionary<uint, DestructibleSpawnBuilder> Spawns { get; } = new();
 
-    private Dictionary<Type, ISpawnerIdentifier> Identifiers { get; } = new();
+    internal Dictionary<Type, ISpawnerIdentifier> Identifiers { get; } = new();
 
-    internal DestructibleSpawnerTemplate Build()
-    {
-        foreach (var spawnBuilder in Spawns)
-        {
-            Template.Spawns[spawnBuilder.Key] = spawnBuilder.Value.Build();
-        }
-
-        Template.Identifiers = Identifiers.Values.ToList();
-
-        return Template;
-    }
+    private BuilderOption<string> TemplateName;
+    private BuilderOption<int?> ConditionMaxCloseCreatures;
+    private BuilderOption<int?> ConditionMaxCreatures;
+    private BuilderOption<float?> ConditionPlayerWithinDistance;
+    private BuilderOption<float?> DistanceConsideredClose;
+    private BuilderOption<float?> DistanceConsideredFar;
+    private BuilderOption<float?> LevelUpChance;
+    private BuilderOption<bool?> OnGroundOnly;
+    private BuilderOption<bool?> Patrol;
+    private BuilderOption<TimeSpan?> SpawnInterval;
+    private BuilderOption<bool> RemoveNotConfiguredSpawns;
 
     public IDestructibleSpawnBuilder GetSpawnBuilder(uint id)
     {
@@ -35,39 +35,90 @@ internal class DestructibleSpawnerBuilder : IDestructibleSpawnerBuilder
         return Spawns[id] = new DestructibleSpawnBuilder(id);
     }
 
+    internal DestructibleSpawnerTemplate Build()
+    {
+        TemplateName.DoIfSet(x => Template.TemplateName = x);
+        ConditionMaxCloseCreatures.DoIfSet(x => Template.ConditionMaxCloseCreatures = x);
+        ConditionMaxCreatures.DoIfSet(x => Template.ConditionMaxCreatures = x);
+        ConditionPlayerWithinDistance.DoIfSet(x => Template.ConditionPlayerWithinDistance = x);
+        DistanceConsideredClose.DoIfSet(x => Template.DistanceConsideredClose = x);
+        DistanceConsideredFar.DoIfSet(x => Template.DistanceConsideredFar = x);
+        LevelUpChance.DoIfSet(x => Template.LevelUpChance = x);
+        OnGroundOnly.DoIfSet(x => Template.OnGroundOnly = x);
+        Patrol.DoIfSet(x => Template.SetPatrol = x);
+        SpawnInterval.DoIfSet(x => Template.SpawnInterval = x);
+        RemoveNotConfiguredSpawns.DoIfSet(x => Template.RemoveNotConfiguredSpawns = x);
+
+        foreach (var spawnBuilder in Spawns)
+        {
+            Template.Spawns[spawnBuilder.Key] = spawnBuilder.Value.Build();
+        }
+
+        Template.Identifiers = Identifiers.Values.ToList();
+
+        return Template;
+    }
+
+    internal void Merge(DestructibleSpawnerBuilder builder)
+    {
+        builder.TemplateName.AssignIfSet(ref TemplateName);
+        builder.ConditionMaxCloseCreatures.AssignIfSet(ref ConditionMaxCloseCreatures);
+        builder.ConditionMaxCreatures.AssignIfSet(ref ConditionMaxCreatures);
+        builder.ConditionPlayerWithinDistance.AssignIfSet(ref ConditionPlayerWithinDistance);
+        builder.DistanceConsideredClose.AssignIfSet(ref DistanceConsideredClose);
+        builder.DistanceConsideredFar.AssignIfSet(ref DistanceConsideredFar);
+        builder.LevelUpChance.AssignIfSet(ref LevelUpChance);
+        builder.OnGroundOnly.AssignIfSet(ref OnGroundOnly);
+        builder.Patrol.AssignIfSet(ref Patrol);
+        builder.SpawnInterval.AssignIfSet(ref SpawnInterval);
+        builder.RemoveNotConfiguredSpawns.AssignIfSet(ref RemoveNotConfiguredSpawns);
+
+        foreach (var spawn in builder.Spawns)
+        {
+            if (Spawns.TryGetValue(spawn.Key, out var existing))
+            {
+                existing.Merge(spawn.Value);
+            }
+            else
+            {
+                Spawns[spawn.Key] = spawn.Value;
+            }
+        }
+    }
+
     public IDestructibleSpawnerBuilder SetTemplateName(string templateName)
     {
-        Template.TemplateName = templateName;
+        TemplateName = new(templateName);
         return this;
     }
 
     public IDestructibleSpawnerBuilder SetConditionMaxCloseCreatures(int? maxCloseCreatures)
     {
-        Template.ConditionMaxCloseCreatures = maxCloseCreatures;
+        ConditionMaxCloseCreatures = new(maxCloseCreatures);
         return this;
     }
 
     public IDestructibleSpawnerBuilder SetConditionMaxCreatures(int? maxCreatures)
     {
-        Template.ConditionMaxCreatures = maxCreatures;
+        ConditionMaxCreatures = new(maxCreatures);
         return this;
     }
 
     public IDestructibleSpawnerBuilder SetConditionPlayerWithinDistance(float? withinDistance)
     {
-        Template.ConditionPlayerWithinDistance = withinDistance;
+        ConditionPlayerWithinDistance = new(withinDistance);
         return this;
     }
 
     public IDestructibleSpawnerBuilder SetDistanceConsideredClose(float? distance)
     {
-        Template.DistanceConsideredClose = distance;
+        DistanceConsideredClose= new(distance);
         return this;
     }
 
     public IDestructibleSpawnerBuilder SetDistanceConsideredFar(float? distance)
     {
-        Template.DistanceConsideredFar = distance;
+        DistanceConsideredFar = new(distance);
         return this;
     }
 
@@ -79,31 +130,31 @@ internal class DestructibleSpawnerBuilder : IDestructibleSpawnerBuilder
 
     public IDestructibleSpawnerBuilder SetLevelUpChance(float? levelUpChance)
     {
-        Template.LevelUpChance = levelUpChance;
+        LevelUpChance = new(levelUpChance);
         return this;
     }
 
     public IDestructibleSpawnerBuilder SetOnGroundOnly(bool? checkGround)
     {
-        Template.OnGroundOnly = checkGround;
+        OnGroundOnly = new(checkGround);
         return this;
     }
 
     public IDestructibleSpawnerBuilder SetPatrol(bool? patrolSpawn)
     {
-        Template.SetPatrol = patrolSpawn;
+        Patrol = new(patrolSpawn);
         return this;
     }
 
     public IDestructibleSpawnerBuilder SetSpawnInterval(TimeSpan? interval)
     {
-        Template.SpawnInterval = interval;
+        SpawnInterval = new(interval);
         return this;
     }
 
     public IDestructibleSpawnerBuilder SetRemoveNotConfiguredSpawns(bool removeNotConfigured)
     {
-        Template.RemoveNotConfiguredSpawns = removeNotConfigured;
+        RemoveNotConfiguredSpawns = new(removeNotConfigured);
         return this;
     }
 }
