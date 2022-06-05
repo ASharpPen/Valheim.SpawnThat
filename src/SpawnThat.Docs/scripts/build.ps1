208dd51c795ae76c828501c7fb4fb3304b745842
Asharppen <asharppen@gmail.com>
2022-06-05 17:20:28 +0200

    Setting up and fixing code versioning

--- src/SpawnThat.Docs/scripts/build.ps1
+++ src/SpawnThat.Docs/scripts/build.ps1
@@ -1,6 +1,16 @@
+# Get old code references
+if (-Not (Test-Path -Path ..\obj\oldcode\1.0.0\))
+{
+  New-Item -ItemType Directory -Force -Path ..\obj\oldcode\1.0.0\
+  git clone -b 1.0.5 --depth 1 https://github.com/ASharpPen/Valheim.SpawnThat.git ..\obj\oldcode\1.0.0\
+}
+
+# Build initial
 docfx metadata ../docfx.json
 
-.\nest-namespaces.ps1 ../obj/api/reference/toc.yml ../obj/api/reference/toc.yml
+# Fix namespace nesting for code docs
+.\nest-namespaces.ps1 ../obj/api/1.0.0/reference/toc.yml ../obj/api/1.0.0/reference/toc.yml
+.\nest-namespaces.ps1 ../obj/api/1.1.0/reference/toc.yml ../obj/api/1.1.0/reference/toc.yml
 
+# Build final
 docfx build ../docfx.json
-
