d1dc7761d14ba35e08e60684cf754ab5bc4e69c8
Asharppen <asharppen@gmail.com>
2023-10-24 21:08:55 +0200

    Minor performance tweak for heavily modded scenarios

--- src/SpawnThat/SpawnThatPlugin.cs
+++ src/SpawnThat/SpawnThatPlugin.cs
@@ -6,6 +6,8 @@ using SpawnThat.Core;
 
 namespace SpawnThat;
 
+// The LocalizationCache is only here to help ordering mods for slightly improved load performance.
+[BepInDependency("com.maxsch.valheim.LocalizationCache", BepInDependency.DependencyFlags.SoftDependency)]
 [BepInDependency("RagnarsRokare.MobAILib", BepInDependency.DependencyFlags.SoftDependency)]
 [BepInDependency("org.bepinex.plugins.creaturelevelcontrol", BepInDependency.DependencyFlags.SoftDependency)]
 [BepInDependency("randyknapp.mods.epicloot", BepInDependency.DependencyFlags.SoftDependency)]
